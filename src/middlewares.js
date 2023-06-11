4c11575c97f8556bd233924912aceb1b6a5a8d40
kes <allinpositive@gmail.com>
2023-06-11 22:24:34 +0900

    noaws

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,18 +1,18 @@
 import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk"
+// import multerS3 from "multer-s3";
+// import aws from "aws-sdk"
 
-const s3 = new aws.S3({
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey:process.env.AWS_SECRET,
-    region: "ap-northeast-2",
-})
+// const s3 = new aws.S3({
+//     accessKeyId: process.env.AWS_ID,
+//     secretAccessKey:process.env.AWS_SECRET,
+//     region: "ap-northeast-2",
+// })
 
-const multerUploader = multerS3({
-    s3: s3,
-    bucket: "kesyoutube", 
-    acl: 'public-read',
-})
+// const multerUploader = multerS3({
+//     s3: s3,
+//     bucket: "kesyoutube", 
+//     acl: 'public-read',
+// })
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -42,13 +42,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
     dest: "uploads/avatars/", limits: {fileSize: 3000000,
     },
-    storage: multerUploader,
+    // storage: multerUploader,
 });
 
 export const videoUpload = multer({
     dest: "uploads/videos/", limits: {fileSize: 10000000000000,
     },
-    storage: multerUploader,
+    // storage: multerUploader,
 })
 
 
